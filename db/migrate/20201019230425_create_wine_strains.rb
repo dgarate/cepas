0a7d8a8d84249e4c303724f43e772c9fc2403e1d
dgarate <garatex@gmail.com>
2020-10-19 22:30:06 -0300

    first commit

---
+++ db/migrate/20201019230425_create_wine_strains.rb
@@ -0,0 +1,10 @@
+class CreateWineStrains < ActiveRecord::Migration[6.0]
+  def change
+    create_table :wine_strains do |t|
+      t.references :wine, null: false, foreign_key: true
+      t.references :strain, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
